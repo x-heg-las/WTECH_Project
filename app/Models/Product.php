143d4b463581d221be435ed421e2462da1144e51
x-heg-las <pato.heg@gmail.com>
2021-11-19 20:55:33 +0100

    card update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'description', 'price', 'stock'];
+
+    public function images()
+    {
+        return DB::table('images')
+                    ->where('product_id', '=', $this->id)
+                    ->get();
+    }
+
+    public function thumbnail()
+    {
+        return DB::table('images')
+                    ->where('product_id', '=', $this->id)
+                    ->take(1)
+                    ->get();
+    }
 }
