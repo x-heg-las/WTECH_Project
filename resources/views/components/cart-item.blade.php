9659755985879b653eb7fdbd5fabdfcac8d8aca9
x-heg-las <pato.heg@gmail.com>
2021-11-19 23:06:56 +0100

    cart item migration

---
+++ resources/views/components/cart-item.blade.php
@@ -0,0 +1,25 @@
+<li class="cart-item row">
+    <div class="image-wrapper col-sm-2">
+        <img src="img/raspberry_pi.png" class="img-fluid align-middle" alt="product">
+    </div>
+    <div class="product-wrapper col-8 container-fluid d-flex align-item-begin align-content-start">
+        <div class="product-description row mb-auto">
+            <a href="#"><p>$item->name</p></a>
+        </div>
+        <div class="product-meta row ">
+            <ul>
+                <li><a href="#"><span class="fa fa-times-circle" aria-hidden="true">Remove</span></a></li>
+                <li><span class="pricetag">123,4$</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="product-quantity-wrapper col-2">
+        <div class="dropdown">
+            <a class="btn dropdown-toggle dropdown-quantity" href="#" data-bs-toggle="dropdown" >1</a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item">2</a></li>
+                <li><a class="dropdown-item">3</a></li>
+            </ul>
+        </div>
+    </div>
+</li>
