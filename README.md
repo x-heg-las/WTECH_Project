fbcc07fe67b8a92d2f7fe9935736614e6399c658
x-heg-las <pato.heg@gmail.com>
2021-11-19 13:55:23 +0100

    Pridany view pre mainpage + styles,head,footer,nav

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 WTECH_Project
+
+#Routing
+
+/ -> Main page
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark container-fluid">
+    <a class="navbar-brand" href="/">PiStore</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mainNavbar">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="profileDropdown" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user-circle fa-lg"></i></a>
+        <ul class="dropdown-menu" aria-labelledby="profileDropdown">
+            <li><span class="dropdown-item">Customer Name</span></li>
+            <li><a class="dropdown-item" href="#">Profile settings</a></li>
+        </ul>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Shopping cart</a>
+        </li>
+    </ul>
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn purple-btn" type="submit" id="searchButton" >Search</button>
+    </form>
+    </div>
+</nav>
