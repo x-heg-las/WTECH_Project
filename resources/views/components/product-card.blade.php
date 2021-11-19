1d51d7a6518a341472e06175920e8042cbe698db
Adam Žák <adam.hrac@gmail.com>
2021-11-19 22:24:27 +0100

    Commit before mrege

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,7 +1,7 @@
 <div class="col-12 col-sm-6 col-md-4 col-lg-3">
     <div class="card product-card text-center">
         <a href="#">
-            <img src="images/{{ $record->thumbnail()->image_source }}" alt="Arduino" class="img-fluid" />
+            // <img src="images/{{ $record->thumbnail()->image_source }}" alt="Arduino" class="img-fluid" />
         </a>
         <section>
             <div class="card-body">
